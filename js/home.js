8cce932ef7ee316ece024ec9f28ec535ae41418e
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2023-11-03 14:36:54 +0400

    completed login and register

--- js/home.js
+++ js/home.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+const navLink = document.getElementsByClassName("nav-link");
+
+
+
+if (window.scrollY > 300) {
+    console.log("oldu");
+    // alert(">>>>")
+    // navLink.classList.add("sticky")
+}
