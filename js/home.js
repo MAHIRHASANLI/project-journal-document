cbf6b0d0536778c9879748e13206559daa7809ef
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2023-11-09 18:03:07 +0400

    completed first section

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
-console.log("Hello world");
-const navLink = document.getElementsByClassName("nav-link");
+// console.log("Hello world");
+// const navLink = document.getElementsByClassName("nav-link");
 
 
 
@@ -17,3 +17,35 @@ const navLink = document.getElementsByClassName("nav-link");
 //         document.getElementById("navbar").style.top = "-1000px";
 //     }
 // }
+const searchInput = document.getElementById("search-input");
+const searchIcon = document.getElementById("search-icon");
+const searchDiv = document.getElementsByClassName("header-search-div");
+
+searchIcon.addEventListener("click", () => {
+    // searchDiv.classList("header-search")
+    searchDiv.style.width = "350px";
+    searchDiv.style.height = "45px";
+    searchDiv.style.padding = "10px 30px";
+    searchDiv.style.border = "1px solid #b9b9b9";
+    // searchInput.style.display = "block"
+})
+
+/* ///////////// */
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
